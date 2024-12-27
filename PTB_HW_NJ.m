52383cf3873d408adf2f0885746a292581db3ddc
Niklas Jocher <150021458+ndjoc@users.noreply.github.com>
2024-12-27 19:46:21 +0100

    Add load images

--- PTB_HW_NJ.m
+++ PTB_HW_NJ.m
@@ -1,6 +1,31 @@
-%% Fixation Cross
-% Set up screen 
+%% Get images 
+% Define directories
+famousDir = '/Users/niklasjocher/Library/CloudStorage/OneDrive-Personal/Dokumente/Universität Salzburg/MSc. Psychologie/Semester 1 WS2425/UE Introduction to scientific programming/PTB_HW/famous_faces/';
+nonFamousDir = '/Users/niklasjocher/Library/CloudStorage/OneDrive-Personal/Dokumente/Universität Salzburg/MSc. Psychologie/Semester 1 WS2425/UE Introduction to scientific programming/PTB_HW/non_famous_faces/'; 
 
+% Get list of PNG files in each folder
+famousFiles = dir(fullfile(famousDir, '*.png'));
+nonFamousFiles = dir(fullfile(nonFamousDir, '*.png'));
+
+% Preallocate cell arrays to store images
+numFamous = numel(famousFiles);
+numNonFamous = numel(nonFamousFiles);
+
+famousImages = cell(1, numFamous);
+nonFamousImages = cell(1, numNonFamous);
+
+% Load famous faces
+for i = 1:numFamous
+    imgPath = fullfile(famousDir, famousFiles(i).name);
+    famousImages{i} = imread(imgPath);
+end
+
+% Load non-famous faces
+for i = 1:numNonFamous
+    imgPath = fullfile(nonFamousDir, nonFamousFiles(i).name);
+    nonFamousImages{i} = imread(imgPath);
+end
+%% fixation cross & mask with time jitter 
 for i = 1:2
     Screen('Preference', 'SkipSyncTests', 1);
     [win, rect] = Screen('OpenWindow', 0, 128, [0 0 640 416]);
@@ -19,8 +44,8 @@ for i = 1:2
     xCenter = rect(3) / 2; yCenter = rect(4) / 2;
     
     % Define square size and position
-    squareSize = 350; % Size of the square in pixels
-    [xCenter, yCenter] = RectCenter(rect ); % Get the center of the window
+    squareSize = 350; % Size of the square
+    [xCenter, yCenter] = RectCenter(rect); % Center the square in the window
     
     % Generate random noise and jitter
     noiseMatrix = rand(squareSize, squareSize) * 255; % Random values between 0-255
@@ -39,6 +64,9 @@ for i = 1:2
     % Wait for the jittered duration
     WaitSecs(jitteredTime);
     
+    % Show picture of famous/non-famous face
+
+    % maybe reaction time
     % Clean up
     Screen('CloseAll');
 end
