dec003191316a88347824a168283838e45daa5b4
Niklas Jocher <150021458+ndjoc@users.noreply.github.com>
2025-01-02 16:57:47 +0100

    Add Instructions

--- PTB_HW_NJ.m
+++ PTB_HW_NJ.m
@@ -1,81 +1,26 @@
-%% Get images 
-% Define directories
-famousDir = '/Users/niklasjocher/Library/CloudStorage/OneDrive-Personal/Dokumente/Universität Salzburg/MSc. Psychologie/Semester 1 WS2425/UE Introduction to scientific programming/PTB_HW/famous_faces/';
-nonFamousDir = '/Users/niklasjocher/Library/CloudStorage/OneDrive-Personal/Dokumente/Universität Salzburg/MSc. Psychologie/Semester 1 WS2425/UE Introduction to scientific programming/PTB_HW/non_famous_faces/'; 
-
-% Get list of PNG files in each folder
-famousFiles = dir(fullfile(famousDir, '*.png'));
-nonFamousFiles = dir(fullfile(nonFamousDir, '*.png'));
-
-% Preallocate cell arrays to store images
-numFamous = numel(famousFiles);
-numNonFamous = numel(nonFamousFiles);
-
-famousImages = cell(1, numFamous);
-nonFamousImages = cell(1, numNonFamous);
-
-% Load famous faces
-for i = 1:numFamous
-    imgPath = fullfile(famousDir, famousFiles(i).name);
-    famousImages{i} = imread(imgPath);
-end
-
-% Load non-famous faces
-for i = 1:numNonFamous
-    imgPath = fullfile(nonFamousDir, nonFamousFiles(i).name);
-    nonFamousImages{i} = imread(imgPath);
-end
-%% fixation cross & mask with time jitter 
-for i = 1:2
-    Screen('Preference', 'SkipSyncTests', 1);
-    [win, rect] = Screen('OpenWindow', 0, 128, [0 0 640 416]);
-    Screen('TextSize', win, 50);
-    xCenter = rect(3) / 2; yCenter = rect(4) / 2;
-
-    % Draw a cross
-    DrawFormattedText(win, '+', 'center', 'center', 255);
-    Screen('Flip', win);
-    WaitSecs(2)
-
-    % Mask 
-    Screen('Preference', 'SkipSyncTests', 1);
-    [win, rect] = Screen('OpenWindow', 0, [128, 128, 128], [0 0 640 416]);
-    Screen('TextSize', win, 50);
-    xCenter = rect(3) / 2; yCenter = rect(4) / 2;
-    
-    % Define square size and position
-    squareSize = 350; % Size of the square
-    [xCenter, yCenter] = RectCenter(rect); % Center the square in the window
-    
-    % Generate random noise and jitter
-    noiseMatrix = rand(squareSize, squareSize) * 255; % Random values between 0-255
-    noiseTexture = Screen('MakeTexture', win, noiseMatrix);
-    minTime = 0.1; 
-    maxTime = 0.9; 
-    jitteredTime = minTime + (maxTime - minTime) * rand;
-    
-    % Define destination rectangle for the noisy square
-    destRect = CenterRectOnPointd([0 0 squareSize squareSize], xCenter, yCenter);
-    
-    % Draw the texture to the screen
-    Screen('DrawTexture', win, noiseTexture, [], destRect);
-    Screen('Flip', win)
-    
-    % Wait for the jittered duration
-    WaitSecs(jitteredTime);
-    
-    % Show picture of famous/non-famous face
-
-    % maybe reaction time
-    % Clean up
-    Screen('CloseAll');
-end
-
-
-%% Attemtping to squeeze everything into one full script (at least working on it)
 Screen('Preference', 'SkipSyncTests', 1); % Skip sync tests for testing purposes
 [win, rect] = Screen('OpenWindow', 0, 128, [0 0 1280 832]);
 [xCenter, yCenter] = RectCenter(rect); % Screen center
+
+% Display Instructions
+instructions = [
+    'Welcome to our experiment.\n\n', ...
+    'You will be presented with pictures of faces.\n\n', ...
+    'If the person in the picture is famous, press "F".\n\n', ...
+    'If the person in the picture is not famous, press "N".\n\n', ...
+    'Press any key to begin.'
+];
+
+% Set text size and color
+Screen('TextSize', win, 24); % Adjust text size if needed
+textColor = [255, 255, 255]; % White text
+
+% Draw the instructions on the screen
+DrawFormattedText(win, instructions, 'center', 'center', textColor);
+Screen('Flip', win);
+
+% Wait for a key press to proceed  
+KbStrokeWait;
 
 % Define keys for responses
 KbName('UnifyKeyNames');
@@ -86,10 +31,9 @@ nonFamousKey = KbName('N'); % Key for non-famous faces
 famousDir = '/Users/niklasjocher/Library/CloudStorage/OneDrive-Personal/Dokumente/Universität Salzburg/MSc. Psychologie/Semester 1 WS2425/UE Introduction to scientific programming/PTB_HW/famous_faces/';
 nonFamousDir = '/Users/niklasjocher/Library/CloudStorage/OneDrive-Personal/Dokumente/Universität Salzburg/MSc. Psychologie/Semester 1 WS2425/UE Introduction to scientific programming/PTB_HW/non_famous_faces/'; 
 
-
 % Get list of images
-famousFiles = dir(fullfile(famousDir, '*.png'));
-nonFamousFiles = dir(fullfile(nonFamousDir, '*.png'));
+famousFiles = dir(fullfile(famousDir, '*.jpg'));
+nonFamousFiles = dir(fullfile(nonFamousDir, '*.jpg'));
 
 % Combine famous and non-famous images
 allFiles = [famousFiles; nonFamousFiles];
@@ -100,8 +44,6 @@ rng('shuffle');
 shuffledIdx = randperm(length(allFiles));
 allFiles = allFiles(shuffledIdx);
 labels = labels(shuffledIdx);
-
-targetSize = [256, 256]
 
 % Loop through trials
 nTrials = length(allFiles);
@@ -139,7 +81,6 @@ for trial = 1:nTrials
     % Load image
     imgPath = fullfile(allFiles(trial).folder, allFiles(trial).name);
     img = imread(imgPath);
-    resizedImg = imresize(img, targetSize);
     texture = Screen('MakeTexture', win, img);
     
     % Display image
